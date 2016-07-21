bd6280332d6f16511bccd58c1755c6429bb136b7
SkidRunner <skidrunner@gmail.com>
2016-07-21 08:50:14 -0500

    Create Function.java

---
+++ critters4j-core/src/main/java/me/skidrunner/critters/data_models/artificial_neural_networks/Function.java
@@ -0,0 +1,33 @@
+package me.skidrunner.critters.data_models.artificial_neural_networks;
+
+public class Function {
+	
+	private int id;
+	private String name;
+	private String className;
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getClassName() {
+		return this.className;
+	}
+	
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	
+}
