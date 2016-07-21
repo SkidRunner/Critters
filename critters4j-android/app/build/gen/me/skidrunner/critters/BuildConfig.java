2f7e876a0c8fcc8d66d19b0b6e9740fcf8e30446
SkidRunner <skidrunner@gmail. com>
2016-07-21 16:21:58 -0500

    add Android project

---
+++ critters4j-android/app/build/gen/me/skidrunner/critters/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package me.skidrunner.critters;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ critters4j-android/app/src/main/java/me/skidrunner/critters/MainActivity.java
@@ -0,0 +1,14 @@
+package me.skidrunner.critters;
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+	
+}
